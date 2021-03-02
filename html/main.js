82a9965806704460114c0d55c2420d9527620d2b
jyoti-wp <sim_s006@icloud.com>
2021-03-02 21:15:46 +0530

    understanding hoisting

--- html/main.js
+++ html/main.js
@@ -7,3 +7,14 @@ function a() {
 }
 
 a();
+
+var favouriteFood = 'Grapes';
+
+var foodThoughts = function () {
+    console.log("Original favourite food: " + "favouriteFood");
+
+    var favouriteFood = 'Sushi';
+
+    console.log("New favourite Food:" + "favouriteFood");
+};
+foodThoughts();
--- html/program.js
+++ html/program.js
@@ -29,6 +29,18 @@ function divide(numOne, numTwo) {
     console.log(result);
 }
 
+var favouriteFood = "Grapes";
+
+var foodThoughts = function () {
+    console.log("Original favourite food: " + "favouriteFood");
+
+    var favouriteFood = 'Sushi';
+
+    console.log("New favourite Food:" + "favouriteFood");
+};
+
+foodThoughts();
+
 
 
 
